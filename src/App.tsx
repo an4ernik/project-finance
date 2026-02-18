cafbefa7dce2906e9e20683a42c094b29e9aca2c
an4ernik <an4ernik2@gmail.com>
2026-02-18 13:20:16 +0200

    bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom"
 import { Toaster } from "sonner"
-import Header from "./components/ui/Header"
 import Home from "./pages/Home"
 import SignUp from "./pages/SignUp"
 import Login from "./pages/Login"
