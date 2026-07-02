94e526079b3d9e4c34bc85c5eeff9e9467bd0df0
an4ernik <an4ernik2@gmail.com>
2026-07-02 16:01:57 +0300

    ready to deploy

--- src/pages/income/CategoriesManager.tsx
+++ src/pages/income/CategoriesManager.tsx
@@ -16,6 +16,7 @@ import {useEffect, useState} from 'react';
 import {useTranslation} from 'react-i18next';
 import {toast} from 'sonner';
 import {isAxiosError} from 'axios';
+
 import {
   getGetCategoriesQueryKey,
   useDeleteCategory,
