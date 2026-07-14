3082faf3d2033a49eaf202b2f80059432ac9975a
an4ernik <an4ernik2@gmail.com>
2026-07-15 01:11:28 +0300

    fixed error message bug

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -66,12 +66,12 @@ function SignUp() {
             .string()
             .min(8, t('auth.errors.tooShort'))
             .max(72, t('auth.errors.tooLong'))
+            .regex(/^\S*$/, t('auth.errors.space'))
             .regex(/^[a-zA-Z0-9!@#$%^&*._\-+=?]*$/, t('auth.errors.latinOnly'))
+            .regex(/[!@#$%^&*._\-+=?]/, t('auth.errors.symbol'))
             .regex(/[A-Z]/, t('auth.errors.uppercase'))
             .regex(/[a-z]/, t('auth.errors.lowercase'))
-            .regex(/[0-9]/, t('auth.errors.number'))
-            .regex(/[!@#$%^&*._\-+=?]/, t('auth.errors.symbol'))
-            .regex(/^\S*$/, t('auth.errors.space')),
+            .regex(/[0-9]/, t('auth.errors.number')),
           confirmPassword: z.string().min(1, t('auth.errors.confirmRequired')),
           fullName: z
             .string()
